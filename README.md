6f4f675e78603af6574e800687401509188db04a
Gerhard Ahrens <Gerhard.Ahrens@lifeprojects.de>
2025-12-09 12:10:43 +0100

    Projektdateien hinzufügen.

---
+++ README.md
@@ -0,0 +1,8 @@
+# PWPF TreeView Demo
+
+![NET](https://img.shields.io/badge/NET-8.0-green.svg)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+![VS2022](https://img.shields.io/badge/Visual%20Studio-2022-white.svg)
+![Version](https://img.shields.io/badge/Version-1.0.2025.0-yellow.svg)]
+
+Das Beispiel zeigt verschiedene Lösungen um ein TreeView in WPF darzustellen.
---
+++ TreeViewDemo/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.cs" company="Lifeprojects.de">
+//     Class: MainWindow
+//     Copyright © Lifeprojects.de yyyy
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>dd.MM.yyyy</date>
+//
+// <summary>
+// Klasse für 
+// </summary>
+//-----------------------------------------------------------------------
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace TreeViewDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            WeakEventManager<Window, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
+
+            this.WindowTitel = "Minimal WPF Template";
+            this.DataContext = this;
+        }
+
+        private string _WindowTitel;
+
+        public string WindowTitel
+        {
+            get { return _WindowTitel; }
+            set
+            {
+                if (this._WindowTitel != value)
+                {
+                    this._WindowTitel = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        #region INotifyPropertyChanged implementierung
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler == null)
+            {
+                return;
+            }
+
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
+        }
+        #endregion INotifyPropertyChanged implementierung
+    }
+}
